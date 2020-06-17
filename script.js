60fde0cf1caf975ada4ea605c10044b1897751cd
Your Name <you@example.com>
2020-06-17 16:57:19 +0200

    testing

---
+++ script.js
@@ -0,0 +1,29 @@
+// function probar(bool) {
+//   return bool;
+// }
+
+// var answer = probar(true) ? "You may enter." : "Access Denied";
+
+// function someThing(bool) {
+//   return bool;
+// }
+// var examen = someThing(true) ? "pass" : "fall";
+
+// function moveCommand(direction) {
+//   var  whatHappens;
+//   switch(direction) {
+//     case"forward":
+//     whatHappens = "you are fine.";
+//     break;
+//     case "back":
+//       whatHappens = " YOU ARE DIED.";
+//       break;
+//       default: 
+//       whatHappens = "there is no hope.";
+//   }
+//    return whatHappens;
+// }
+
+function bb() {
+  var a = "Hello"
+}
